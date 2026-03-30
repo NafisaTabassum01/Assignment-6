405136b825028d01470ee0d4c6e64b6c26ea539a
NafisaTabassum01 <tabassumnafisa28.01@gmail.com>
2026-03-31 03:08:25 +0600

    banner section ongoing

---
+++ src/firstPart/HeroSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const HeroSection = () => {
+    return (
+        <div>
+      <div className='flex'>
+        <div><p className='font-bold text-6xl'>Supercharge Your Digital Workflow</p>
+        <p className='text-gray-500 text-[18px]'>Access premium AI tools, design assets, templates, and productivity
+software—all in one place. Start creating faster today.
+
+Explore Products
+</p>
+<button className='btn rounded-3xl'>Explore Products</button>
+<button className='btn border border-purple-600 rounded-3xl bg-white bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-semi-bold '>Watch Demo</button>
+        </div>
+        <div><img src="" alt="" /></div>
+      </div>
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar bg-base-100 w-10/12 mx-auto border-none">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -20,29 +20,30 @@ const Navbar = () => {
         <li><a>FAQ</a></li>
       </ul>
     </div>
-    <p className="text-4xl font-bold bg-gradient-to-r from-[#4F39F6] to-[#9514FA] p-2 bg-clip-text text-transparent">DigiTools</p>
+    <p className="text-4xl font-bold bg-linear-to-r from-[#4F39F6] to-[#9514FA] p-2 bg-clip-text text-transparent">DigiTools</p>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
       <li>
-        <a>Products</a>
+        <a className='font-semibold text-[16px]'>Products</a>
         </li>
       <li>
-        <a>Features</a>
+        <a className='font-semibold text-[16px]'>Features</a>
         </li>
       <li>
-        <a>Pricing</a>
+        <a className='font-semibold text-[16px]'>Pricing</a>
         </li>
-      
-    
-      <li><a>Testimonials</a></li>
-      <li><a>FAQ</a></li>
+    <li>
+        <a className='font-semibold text-[16px]'>Testimonials</a>
+        </li>
+      <li><a className='font-semibold text-[16px]'>FAQ</a></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-3xl">Get Started</a>
   </div>
 </div>
+<hr className='border border-gray-200' />
         </div>
            
 
